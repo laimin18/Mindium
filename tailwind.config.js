3626bb51d9f4ea26de68a4e2ffacfacd630606e2
Min <a950897@gmail.com>
2022-12-23 23:49:55 +0800

    tailwind css i/o files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./server/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
